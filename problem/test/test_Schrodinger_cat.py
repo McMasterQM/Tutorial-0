734063465b6697006bb04b155a5e7a79198b2304
RichRick1 <36164007+RichRick1@users.noreply.github.com>
2022-01-11 16:12:57 -0500

    comment test for False and False

--- problem/test/test_Schrodinger_cat.py
+++ problem/test/test_Schrodinger_cat.py
@@ -6,4 +6,4 @@ def test_schrodinger_cat():
     :return:
     """
     is_dead, is_alive = schrodinger_cat()
-    assert (is_dead and is_alive is True) or (is_dead or is_alive is False)
+    assert (is_dead and is_alive is True) or (is_dead or is_alive is False) ## (True and True) or (False and False)
